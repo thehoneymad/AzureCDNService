d93ae5f425bc82fc9bfc6468a977f6ff1295ac00
Swagata Prateek <s.prateek@hotmail.com>
2016-01-01 17:12:38 +0600

    MINOR: Renamed WebApp.start return

--- AzureCDNService/AzureCDNService/Program.cs
+++ AzureCDNService/AzureCDNService/Program.cs
@@ -16,7 +16,7 @@ namespace AzureCDNService
             options.Urls.Add("http://localhost:1337");
             options.Urls.Add("http://127.0.0.1:1337");
 
-            var SleipnirWebApi = WebApp.Start<Startup>(options);
+            var AzureCDNWebApiService = WebApp.Start<Startup>(options);
 
             Console.WriteLine("Started Azure CDN Upload Server at " + options.Urls.Last());
             Console.WriteLine("Press Any Key To Exit");
